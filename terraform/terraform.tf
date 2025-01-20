f607eff7e944b4cefc5edf9685b8d9c67f081974
varmaaradhula <rkvarmaa@gmail.com>
2025-01-20 22:31:04 +0000

    intial commit

---
+++ terraform/terraform.tf
@@ -0,0 +1,40 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.25.0"
+    }
+
+    random = {
+      source  = "hashicorp/random"
+      version = "~> 3.5.1"
+    }
+
+    tls = {
+      source  = "hashicorp/tls"
+      version = "~> 4.0.4"
+    }
+
+    cloudinit = {
+      source  = "hashicorp/cloudinit"
+      version = "~> 2.3.2"
+    }
+
+    kubernetes = {
+      source  = "hashicorp/kubernetes"
+      version = "~> 2.23.0"
+    }
+  }
+
+  backend "s3" {
+    bucket = "vprofileactions07"
+    key    = "terraform.tfstate"
+    region = "eu-west-2"
+  }
+
+  #required_version = "~> 1.6.3"
+  required_version = ">= 1.4.0"
+}
+##
+##
+##
---
+++ terraform/variables.tf
@@ -0,0 +1,15 @@
+variable "region" {
+  description = "AWS region"
+  type        = string
+  default     = "eu-west-2"
+}
+
+variable "clusterName" {
+  description = "Name of the EKS cluster"
+  type        = string
+  default     = "vprofile-eks"
+}
+
+## ## ### ###### #####
+####### #######
+# This varma
