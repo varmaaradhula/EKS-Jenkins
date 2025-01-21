ddbe40f05a543a2a432c2c66ae71c18833923893
varmaaradhula <rkvarmaa@gmail.com>
2025-01-21 00:36:14 +0000

    fixing issues

--- terraform/eks-cluster.tf
+++ terraform/eks-cluster.tf
@@ -37,10 +37,6 @@ module "eks" {
   }
 }
 
-output "cluster_endpoint" {
-  value = module.eks.cluster_endpoint
-}
-
 output "cluster_certificate_authority_data" {
   value = module.eks.cluster_certificate_authority_data
 }
